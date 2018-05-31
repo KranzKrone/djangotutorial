d4fb2e4ed1457e50d2e597602c7ba21811ad820d
Stefan <dev@st-page.de>
2018-05-31 19:14:45 +0200

    Testkommentar

--- polls/views.py
+++ polls/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 
 from django.http import HttpResponse
 
-
+# Testkommentar
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
